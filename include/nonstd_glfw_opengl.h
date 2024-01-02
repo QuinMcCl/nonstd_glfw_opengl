b5e33d63f349176c4415c759e205de41964945ad
QuinMccl <herobrinemcq@gmail.com>
2024-01-01 19:16:39 -0500

    c++ name mangling

--- include/nonstd_glfw_opengl.h
+++ include/nonstd_glfw_opengl.h
@@ -1,6 +1,10 @@
 #ifndef NONSTD_GLFW_OPENGL_H
 #define NONSTD_GLFW_OPENGL_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 #include <GL/glew.h>
 #include <GL/glut.h>
@@ -8,5 +12,9 @@
 #include "nonstd_glfw_event.h"
 #include "nonstd_glfw.h"
 #include "nonstd_glfw_window.h"
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
 
 #endif
